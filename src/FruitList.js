ede8ec4f4787bd9004df5d8813ef408dad8cfb3f
haidar <haidarprayoga9b@gmail.com>
2024-09-25 02:16:37 +0700

    first commit

---
+++ src/FruitList.js
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import "./index.css";
+
+const ThemeToggle = () => {
+  const [theme, setTheme] = useState("light");
+
+  // SetTheme first
+  useEffect(() => {
+    const savedTheme = localStorage.getItem("theme") || "light";
+    setTheme(savedTheme);
+    document.documentElement.setAttribute("data-theme", savedTheme);
+  }, []);
+
+  // Toggle func for theme
+  const toggleTheme = () => {
+    const newTheme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+    document.documentElement.setAttribute("data-theme", newTheme);
+    localStorage.setItem("theme", newTheme);
+  };
+  return (
+    <button onClick={toggleTheme}>
+      {theme === "light" ? "Switch to Dark Mode" : "Switch to Light Mode"}
+    </button>
+  );
+};
+
+export default ThemeToggle;
